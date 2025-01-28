5a67520129babb78206db9b95c5cb42d38e9589a
rahulsw00 <rahulsw12234@gmail.com>
2025-01-28 10:27:43 +0530

    new

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,9 +55,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "api",
-    "rest_framework",
-    "corsheaders",
 ]
 
 MIDDLEWARE = [
